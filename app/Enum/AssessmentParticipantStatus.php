bcb0025b3c8c3da17efd12459ece85bfe644d3f1
risyalf <risyalfebrianto97@gmail.com>
2026-01-18 16:17:50 +0700

    tambah status partisipan tes

--- app/Enum/AssessmentParticipantStatus.php
+++ app/Enum/AssessmentParticipantStatus.php
@@ -8,6 +8,7 @@ enum AssessmentParticipantStatus: string
 {
     use EnumOptions;
 
+    case LOGGED_IN = 'Log In';
     case ACTIVE = 'Aktif';
     case PAUSED = 'Hentikan Sementara';
     case LOCKED = 'Terkunci';
