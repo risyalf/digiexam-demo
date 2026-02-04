9211314b4ecb424f2c42dd84f07e7aa5f6bdb4bc
risyalf <risyalfebrianto97@gmail.com>
2026-02-04 08:53:50 +0700

    fix

--- app/Action/ExportTestFormDocx.php
+++ app/Action/ExportTestFormDocx.php
@@ -4,28 +4,19 @@ namespace App\Action;
 
 use PhpOffice\PhpWord\IOFactory;
 use PhpOffice\PhpWord\PhpWord;
+use PhpOffice\PhpWord\SimpleType\Jc;
 
 class ExportTestFormDocx
 {
     public static function execute($fileName)
     {
         return response()->streamDownload(function () {
-            $styleBorderTopCell = [
-                'borderTopSize' => 12,
-                'borderTopColor' => '000000',
-            ];
+            $styleBorderTopCell = ['borderTopSize' => 12, 'borderTopColor' => '000000',];
+            $styleBorderBottomCell = ['borderBottomSize' => 12, 'borderBottomColor' => '000000',];
+            $styleBorderLeftRightCell = ['borderLeftSize' => 12, 'borderLeftColor' => '000000', 'borderRightSize' => 12, 'borderRightColor' => '000000',];
 
-            $styleBorderBottomCell = [
-                'borderBottomSize' => 12,
-                'borderBottomColor' => '000000',
-            ];
-
-            $styleBorderLeftRightCell = [
-                'borderLeftSize' => 12,
-                'borderLeftColor' => '000000',
-                'borderRightSize' => 12,
-                'borderRightColor' => '000000',
-            ];
+            $centeredPara = ['alignment' => Jc::CENTER];
+            $valignCenter = ['valign' => 'center'];
 
             $jumlahSoal = 50;
             $phpWord = new PhpWord;
@@ -41,34 +32,21 @@ class ExportTestFormDocx
             for ($i = 1; $i <= $jumlahSoal; $i++) {
 
                 $table->addRow();
-                $table->addCell(1000, [...$styleBorderTopCell, ...$styleBorderLeftRightCell])->addText($i);
+                $table->addCell(1000, [...$styleBorderTopCell, ...$styleBorderLeftRightCell, ...$valignCenter, 'vMerge' => 'restart'])
+                    ->addText($i, null, $centeredPara);
                 $table->addCell(2000, [...$styleBorderTopCell, ...$styleBorderLeftRightCell])->addText("PILIHAN GANDA / ESSAY");
                 $table->addCell(4000, [...$styleBorderTopCell, ...$styleBorderLeftRightCell])->addText($i == 1 ? "(MASUKKAN PERTANYAAN DAN GAMBAR DI SINI)" : "");
                 $table->addCell(2000, [...$styleBorderTopCell, ...$styleBorderLeftRightCell, 'bgColor' => 'FFFF00'])->addText("");
 
-                $table->addRow();
-                $table->addCell(1000, $styleBorderLeftRightCell)->addText("");
-                $table->addCell(2000, $styleBorderLeftRightCell)->addText("JAWABAN");
-                $table->addCell(4000, $styleBorderLeftRightCell)->addText($i == 1 ? "(MASUKKAN OPSI PERTANYAAN ATAU KOSONGI)" : "");
-                $table->addCell(2000, $styleBorderLeftRightCell)->addText($i == 1 ? "(MASUKKAN ANGKA '1' JIKA JAWABAN BENAR, '0' JIKA SALAH)" : "");
+                for ($j = 1; $j <= 4; $j++) {
+                    $currentBorderStyle = [...$styleBorderBottomCell, ...$styleBorderTopCell];
 
-                $table->addRow();
-                $table->addCell(1000, $styleBorderLeftRightCell)->addText("");
-                $table->addCell(2000, $styleBorderLeftRightCell)->addText("JAWABAN");
-                $table->addCell(4000, $styleBorderLeftRightCell)->addText($i == 1 ? "(MASUKKAN OPSI PERTANYAAN ATAU KOSONGI)" : "");
-                $table->addCell(2000, $styleBorderLeftRightCell)->addText($i == 1 ? "(MASUKKAN ANGKA '1' JIKA JAWABAN BENAR, '0' JIKA SALAH)" : "");
-
-                $table->addRow();
-                $table->addCell(1000, $styleBorderLeftRightCell)->addText("");
-                $table->addCell(2000, $styleBorderLeftRightCell)->addText("JAWABAN");
-                $table->addCell(4000, $styleBorderLeftRightCell)->addText($i == 1 ? "(MASUKKAN OPSI PERTANYAAN ATAU KOSONGI)" : "");
-                $table->addCell(2000, $styleBorderLeftRightCell)->addText($i == 1 ? "(MASUKKAN ANGKA '1' JIKA JAWABAN BENAR, '0' JIKA SALAH)" : "");
-
-                $table->addRow();
-                $table->addCell(1000, [...$styleBorderBottomCell, ...$styleBorderLeftRightCell])->addText("");
-                $table->addCell(2000, [...$styleBorderBottomCell, ...$styleBorderLeftRightCell])->addText("JAWABAN");
-                $table->addCell(4000, [...$styleBorderBottomCell, ...$styleBorderLeftRightCell])->addText($i == 1 ? "(MASUKKAN OPSI PERTANYAAN ATAU KOSONGI)" : "");
-                $table->addCell(2000, [...$styleBorderBottomCell, ...$styleBorderLeftRightCell])->addText($i == 1 ? "(MASUKKAN ANGKA '1' JIKA JAWABAN BENAR, '0' JIKA SALAH)" : "");
+                    $table->addRow();
+                    $table->addCell(1000, [...$styleBorderLeftRightCell, ...$currentBorderStyle, 'vMerge' => 'continue']);
+                    $table->addCell(2000, [...$styleBorderLeftRightCell, ...$currentBorderStyle])->addText("JAWABAN");
+                    $table->addCell(4000, [...$styleBorderLeftRightCell, ...$currentBorderStyle])->addText($i == 1 ? "(MASUKKAN OPSI PERTANYAAN ATAU KOSONGI)" : "");
+                    $table->addCell(2000, [...$styleBorderLeftRightCell, ...$currentBorderStyle])->addText($i == 1 ? "(MASUKKAN ANGKA '1' JIKA JAWABAN BENAR, '0' ATAU KOSONGI JIKA SALAH)" : "");
+                }
             }
 
             $writer = IOFactory::createWriter($phpWord);
