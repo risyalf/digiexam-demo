952cfb1fb99dbdedd5ddf4e35b60b34c598754d1
risyalf <risyalfebrianto97@gmail.com>
2026-02-04 21:06:33 +0700

    ganti ke resource

---
+++ app/Filament/Resources/Topics/TopicResource.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Filament\Resources\Topics;
+
+use App\Enum\Menu;
+use App\Filament\Resources\Topics\Pages\ManageTopics;
+use App\Models\Module;
+use App\Models\Topic;
+use BackedEnum;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ForceDeleteAction;
+use Filament\Actions\ForceDeleteBulkAction;
+use Filament\Actions\RestoreAction;
+use Filament\Actions\RestoreBulkAction;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\TrashedFilter;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use UnitEnum;
+
+class TopicResource extends Resource
+{
+    protected static ?string $model = Topic::class;
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+
+    protected static string|UnitEnum|null $navigationGroup = Menu::DATA_MODUL;
+
+    protected static ?string $navigationLabel = "Menu Topik";
+
+    public static function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Select::make('module_id')
+                    ->options(
+                        Module::query()
+                            ->pluck('name', 'id')
+                    )
+                    ->required(),
+                TextInput::make('name')
+                    ->label('NAMA TOPIK')
+                    ->required(),
+                TextInput::make('description')
+                    ->label('DESKRIPSI'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('no')
+                    ->label('NO.')
+                    ->rowIndex(),
+                TextColumn::make('module.name')
+                    ->label('NAMA MODUL')
+                    ->alignCenter(),
+                TextColumn::make('name')
+                    ->label('NAMA')
+                    ->alignCenter(),
+                TextColumn::make('description')
+                    ->label('DESKRIPSI')
+                    ->alignCenter(),
+                TextColumn::make('created_at')
+                    ->label('DIBUAT PADA')
+                    ->alignCenter(),
+            ])
+            ->filters([
+                TrashedFilter::make(),
+            ])
+            ->recordActions([
+                EditAction::make(),
+                DeleteAction::make(),
+                ForceDeleteAction::make(),
+                RestoreAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                    ForceDeleteBulkAction::make(),
+                    RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ManageTopics::route('/'),
+        ];
+    }
+
+    public static function getRecordRouteBindingEloquentQuery(): Builder
+    {
+        return parent::getRecordRouteBindingEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            // 'module'
+        ];
+    }
+}
