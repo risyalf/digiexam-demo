adf61ae60e9d2bf5c574911a6cb619b49138d6f4
risyalf <risyalfebrianto97@gmail.com>
2026-02-12 11:28:48 +0700

    fix api

--- routes/api.php
+++ routes/api.php
@@ -21,25 +21,26 @@ Route::middleware("auth:sanctum")->group(function () {
         "api.user.unlock",
     );
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::prefix('user')->group(function() {
-        Route::get('/{id}', [UserController::class, 'get'])->name('api.user.get');
-    });
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::prefix('user')->group(function () {
+            Route::get('/{id}', [UserController::class, 'get'])->name('api.user.get');
+        });
 
-    Route::prefix('participant')->group(function() {
-        Route::get('/{id}', [ParticipantController::class, 'get'])->name('api.participant.get');
-        Route::post('/lock', [ParticipantController::class, 'lock'])->name('api.participant.lock');
-        Route::post('/unlock', [ParticipantController::class, 'unlock'])->name('api.participant.unlock');
-    });
+        Route::prefix('participant')->group(function () {
+            Route::get('/{id}', [ParticipantController::class, 'get'])->name('api.participant.get');
+            Route::post('/lock', [ParticipantController::class, 'lock'])->name('api.participant.lock');
+            Route::post('/unlock', [ParticipantController::class, 'unlock'])->name('api.participant.unlock');
+        });
 
-    Route::prefix('assessment')->group(function() {
-        Route::get('/', [AssessmentController::class, 'get'])->name('api.assessment.get');
-        Route::post('/', [AssessmentController::class, 'update'])->name('api.assessment.update');
-        Route::post('/join', [AssessmentController::class, 'join'])->name('api.assessment.join');
-        Route::post("/assessment/submit", [AssessmentController::class, "submit"])->name('api.assessment.submit');
-        Route::get("/assessment/result/{assessmentId}/{participantId}", [
-            AssessmentController::class,
-            "result",
-        ])->name('api.assessment.result');
+        Route::prefix('assessment')->group(function () {
+            Route::get('/', [AssessmentController::class, 'get'])->name('api.assessment.get');
+            Route::post('/', [AssessmentController::class, 'update'])->name('api.assessment.update');
+            Route::post('/join', [AssessmentController::class, 'join'])->name('api.assessment.join');
+            Route::post("/assessment/submit", [AssessmentController::class, "submit"])->name('api.assessment.submit');
+            Route::get("/assessment/result/{assessmentId}/{participantId}", [
+                AssessmentController::class,
+                "result",
+            ])->name('api.assessment.result');
+        });
     });
 });
