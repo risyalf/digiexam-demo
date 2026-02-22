c16ee777cb55f362f977f665d8ec1a517b3dc3b1
andywijaya15 <andykevonata@gmail.com>
2026-02-22 17:08:25 +0700

    add order_number and validation when create

--- app/Filament/Resources/Participants/Pages/ManageParticipants.php
+++ app/Filament/Resources/Participants/Pages/ManageParticipants.php
@@ -3,8 +3,11 @@
 namespace App\Filament\Resources\Participants\Pages;
 
 use App\Filament\Resources\Participants\ParticipantResource;
+use App\Models\Participant;
 use Filament\Actions\CreateAction;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ManageRecords;
+use Illuminate\Database\QueryException;
 
 class ManageParticipants extends ManageRecords
 {
@@ -12,6 +15,44 @@ class ManageParticipants extends ManageRecords
 
     protected function getHeaderActions(): array
     {
-        return [CreateAction::make()->label("Tambah Peserta")];
+        return [
+            CreateAction::make()
+                ->label("Tambah Peserta")
+                ->before(function (CreateAction $action) {
+                    $data = $action->getData();
+                    $existsOrderNumber = Participant::query()
+                        ->where(
+                            "participant_group_id",
+                            $data["participant_group_id"],
+                        )
+                        ->where("order_number", $data["order_number"])
+                        ->where("module_id", $data["module_id"])
+                        ->exists();
+                    $existsData = Participant::query()
+                        ->where("user_id", $data["user_id"])
+                        ->where(
+                            "participant_group_id",
+                            $data["participant_group_id"],
+                        )
+                        ->where("module_id", $data["module_id"])
+                        ->exists();
+                    if ($existsOrderNumber) {
+                        Notification::make()
+                            ->warning()
+                            ->title(
+                                "Nomor Urut sudah dipakai,cek Kelas,No Urut dan Modul",
+                            )
+                            ->send();
+                        $action->halt();
+                    }
+                    if ($existsData) {
+                        Notification::make()
+                            ->warning()
+                            ->title("Data sudah ada")
+                            ->send();
+                        $action->halt();
+                    }
+                }),
+        ];
     }
 }
