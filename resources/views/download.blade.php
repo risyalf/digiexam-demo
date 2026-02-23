1b042b13ff7bc2b9d98595af7207619fa013c113
risyalf <risyalfebrianto97@gmail.com>
2026-02-23 08:08:23 +0700

    hilangkan cdn

--- resources/views/download.blade.php
+++ resources/views/download.blade.php
@@ -4,8 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Download Lembar Jawab</title>
-
-    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body class="bg-gray-100 flex flex-col items-center justify-center min-h-screen">
