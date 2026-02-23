5069a03cf53f70be32aaa72ed321f625598c7816
risyalf <risyalfebrianto97@gmail.com>
2026-02-24 05:57:21 +0700

    fix apk

--- resources/views/download.blade.php
+++ resources/views/download.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,39 +11,51 @@
     <h1 class="text-2xl font-semibold mb-12">Aplikasi Lembar Jawab Siswa</h1>
 
     <div class="bg-white py-16 rounded-xl shadow-lg text-center w-full max-w-sm">
-    <div class="mb-8">
-        <a href="{{ route('download.apk.filename', 'assessment_full.apk') }}"
-           class="bg-blue-600 text-white px-6 py-3 rounded-lg text-lg font-medium hover:bg-blue-700 transition">
-            Download
-        </a>
-        <br>
-        <div class="mt-3">
-            (Ukuran : 49 MB. Untuk Versi Full)
+        <div class="mb-8">
+            <a href="{{ route('download.apk.filename', 'assessment_full.apk') }}"
+                class="bg-blue-600 text-white px-6 py-3 rounded-lg text-lg font-medium hover:bg-blue-700 transition">
+                Download
+            </a>
+            <br>
+            <div class="mt-3">
+                (Ukuran : 49 MB. Untuk Versi Full)
+            </div>
         </div>
-    </div>
-    <br>
+        <br>
 
-    <div class="mb-8">
-        <a href="{{ route('download.apk.filename', 'assessment_9_15.apk') }}"
-           class="bg-blue-600 text-white px-6 py-3 rounded-lg text-lg font-medium hover:bg-blue-700 transition">
-            Download
-        </a>
-        <br>
-        <div class="mt-3">
-            (Ukuran : 18 MB. Untuk Versi Android Baru)
+        <div class="mb-8">
+            <a href="{{ route('download.apk.filename', 'assessment_v_new.apk') }}"
+                class="bg-blue-600 text-white px-6 py-3 rounded-lg text-lg font-medium hover:bg-blue-700 transition">
+                Download
+            </a>
+            <br>
+            <div class="mt-3">
+                (Ukuran : 18 MB. Untuk Versi Android Baru)
+            </div>
         </div>
-    </div>
-    <br>
-    <div class="">
-        <a href="{{ route('download.apk.filename', 'assessment_1_8.apk') }}"
-           class="bg-blue-600 text-white px-6 py-3 rounded-lg text-lg font-medium hover:bg-blue-700 transition">
-            Download
-        </a>
         <br>
-        <div class="mt-3">
-            (Ukuran : 15 MB. Untuk Versi Android Lama)
+        <div class="">
+            <a href="{{ route('download.apk.filename', 'assessment_v_old.apk') }}"
+                class="bg-blue-600 text-white px-6 py-3 rounded-lg text-lg font-medium hover:bg-blue-700 transition">
+                Download
+            </a>
+            <br>
+            <div class="mt-3">
+                (Ukuran : 15 MB. Untuk Versi Android Lama)
+            </div>
         </div>
-    </div>
+        <br>
+        <div class="">
+            <a href="{{ route('download.apk.filename', 'assessment_86.apk') }}"
+                class="bg-blue-600 text-white px-6 py-3 rounded-lg text-lg font-medium hover:bg-blue-700 transition">
+                Download
+            </a>
+            <br>
+            <div class="mt-3">
+                (Ukuran : 18 MB. Untuk Versi Android Arsitektur 86)
+            </div>
+        </div>
     </div>
 </body>
+
 </html>
