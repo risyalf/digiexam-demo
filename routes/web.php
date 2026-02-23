bff4ca9f87111077db69b6b000736c942dda56d1
risyalf <risyalfebrianto97@gmail.com>
2026-02-23 21:51:40 +0700

    change web

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,13 @@ Route::get('/', function () {
     return Redirect('admin/login');
 });
 
-Route::get('/download', function () {
-    return view('download');
-});
-
 Route::get('/login', function () {
     return redirect('/admin/login');
 })->name('login');
+
+Route::get('/download', function () {
+    return view('download');
+});
 
 Route::get('/download/apk/{filename}', function ($filename) {
     if (! preg_match('/^[a-zA-Z0-9._-]+\.apk$/', $filename)) {
