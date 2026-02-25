c10d9356d826ba92013480a8b368c7a2bac202fd
risyalf <risyalfebrianto97@gmail.com>
2026-02-26 04:40:12 +0700

    update apk. sertakan build laam

--- resources/views/download.blade.php
+++ resources/views/download.blade.php
@@ -10,9 +10,11 @@
 <body class="bg-gray-100 flex flex-col items-center justify-center min-h-screen">
     <h1 class="text-2xl font-semibold mb-12">Aplikasi Lembar Jawab Siswa</h1>
 
+    <br>
+    <h1 class="text-xl font-semibold mb-12">Build Baru</h1>
     <div class="bg-white py-16 rounded-xl shadow-lg text-center w-full max-w-sm">
         <div class="mb-8">
-            <a href="{{ route('download.apk.filename', 'assessment_full.apk') }}"
+            <a href="{{ route('download.apk.new.filename', 'assessment_full.apk') }}"
                 class="bg-blue-600 text-white px-6 py-3 rounded-lg text-lg font-medium hover:bg-blue-700 transition">
                 Download
             </a>
@@ -28,7 +30,7 @@
         <br>
 
         <div class="mb-8">
-            <a href="{{ route('download.apk.filename', 'assessment_v_new.apk') }}"
+            <a href="{{ route('download.apk.new.filename', 'assessment_v_new.apk') }}"
                 class="bg-blue-600 text-white px-6 py-3 rounded-lg text-lg font-medium hover:bg-blue-700 transition">
                 Download
             </a>
@@ -43,7 +45,57 @@
         </div>
         <br>
         <div class="">
-            <a href="{{ route('download.apk.filename', 'assessment_v_old.apk') }}"
+            <a href="{{ route('download.apk.new.filename', 'assessment_v_old.apk') }}"
+                class="bg-blue-600 text-white px-6 py-3 rounded-lg text-lg font-medium hover:bg-blue-700 transition">
+                Download
+            </a>
+            <br>
+            <div class="mt-3">
+                (Ukuran : 15 MB)
+            </div>
+            <div>
+                Mendukung Android 5.0 Lollipop dan 5.1, untuk perangkat yang masih memakai HP generasi lama.
+            </div>
+            <br>
+        </div>
+        <br>
+    </div>
+    <br>
+    <h1 class="text-xl font-semibold mb-12">Build Lama</h1>
+    <div class="bg-white py-16 rounded-xl shadow-lg text-center w-full max-w-sm">
+        <div class="mb-8">
+            <a href="{{ route('download.apk.old.filename', 'assessment_full.apk') }}"
+                class="bg-blue-600 text-white px-6 py-3 rounded-lg text-lg font-medium hover:bg-blue-700 transition">
+                Download
+            </a>
+            <br>
+            <div class="mt-3">
+                (Ukuran : 49 MB)
+            </div>
+            <div>
+                Versi full, direkomendasikan untuk HP keluaran 2017 ke atas, kompatibel untuk HP Android 6 sampai Android 15
+            </div>
+            <br>
+        </div>
+        <br>
+
+        <div class="mb-8">
+            <a href="{{ route('download.apk.old.filename', 'assessment_v_new.apk') }}"
+                class="bg-blue-600 text-white px-6 py-3 rounded-lg text-lg font-medium hover:bg-blue-700 transition">
+                Download
+            </a>
+            <br>
+            <div class="mt-3">
+                (Ukuran : 18 MB)
+            </div>
+            <div>
+                Versi ringan, direkomendasikan untuk semua HP yang rilis di atas 2015, kompatibel untuk HP Android 6 sampai Android 15
+            </div>
+            <br>
+        </div>
+        <br>
+        <div class="">
+            <a href="{{ route('download.apk.old.filename', 'assessment_v_old.apk') }}"
                 class="bg-blue-600 text-white px-6 py-3 rounded-lg text-lg font-medium hover:bg-blue-700 transition">
                 Download
             </a>
--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,26 @@ Route::get('/download', function () {
     return view('download');
 });
 
-Route::get('/download/apk/{filename}', function ($filename) {
+Route::get('/download/apk/new/{filename}', function ($filename) {
     if (! preg_match('/^[a-zA-Z0-9._-]+\.apk$/', $filename)) abort(400);
     
-    $apkPath = public_path("apk/$filename");
+    $apkPath = public_path("apk/new/$filename");
     if (! File::exists($apkPath)) abort(404);
 
     return response()->download($apkPath, null,[
         'Content-Type' => 'application/vnd.android.package-archive',
         'Content-Disposition' => "attachment; filename=\"$filename\"",
     ]);
-})->name('download.apk.filename');
+})->name('download.apk.new.filename');
+
+Route::get('/download/apk/old/{filename}', function ($filename) {
+    if (! preg_match('/^[a-zA-Z0-9._-]+\.apk$/', $filename)) abort(400);
+    
+    $apkPath = public_path("apk/old/$filename");
+    if (! File::exists($apkPath)) abort(404);
+
+    return response()->download($apkPath, null,[
+        'Content-Type' => 'application/vnd.android.package-archive',
+        'Content-Disposition' => "attachment; filename=\"$filename\"",
+    ]);
+})->name('download.apk.old.filename');
