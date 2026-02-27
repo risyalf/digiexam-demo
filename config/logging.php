040dc0f7596840ba8499466f0f39ab540f81cf4d
andywijaya15 <andykevonata@gmail.com>
2026-02-27 08:23:59 +0700

    fix log

--- config/logging.php
+++ config/logging.php
@@ -128,5 +128,10 @@ return [
         "emergency" => [
             "path" => storage_path("logs/laravel.log"),
         ],
+
+        "deprecations" => [
+            "driver" => "single",
+            "path" => storage_path("logs/php-deprecation-warnings.log"),
+        ],
     ],
 ];
