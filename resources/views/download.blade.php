9ca5e95a5116908d635e6a2a1a82b938ef275888
risyalf <risyalfebrianto97@gmail.com>
2026-03-03 08:09:42 +0700

    tambah link download

--- resources/views/download.blade.php
+++ resources/views/download.blade.php
@@ -14,6 +14,20 @@
     <h1 class="text-xl font-semibold mb-12">Build Baru</h1>
     <div class="bg-white py-16 rounded-xl shadow-lg text-center w-full max-w-sm">
         <div class="">
+            <a href="https://github.com/alris-dev/student-assessment-releases/releases/tag/build-webview"
+                target="_blank"
+                class="bg-blue-600 text-white px-6 py-3 rounded-lg text-lg font-medium hover:bg-blue-700 transition">
+                Link All
+            </a>
+            <br>
+            <div>
+                Semua versi apk (versi baru)
+            </div>
+            <br>
+        </div>
+        <br>
+
+        <div class="">
             <a href="https://github.com/alris-dev/student-assessment-releases/releases/tag/build"
                 target="_blank"
                 class="bg-blue-600 text-white px-6 py-3 rounded-lg text-lg font-medium hover:bg-blue-700 transition">
@@ -21,13 +35,13 @@
             </a>
             <br>
             <div>
-                Semua versi apk
+                Semua versi apk (versi lama)
             </div>
             <br>
         </div>
         <br>
 
-        <div class="mb-8">
+        {{-- <div class="mb-8">
             <a href="https://drive.google.com/file/d/13w_AOINt8D5rfb6v3Y852WaMkEupaCt1/view?usp=drive_link"
                 target="_blank"
                 class="bg-blue-600 text-white px-6 py-3 rounded-lg text-lg font-medium hover:bg-blue-700 transition">
@@ -74,7 +88,7 @@
                 Mendukung Android 5.0 Lollipop dan 5.1, untuk perangkat yang masih memakai HP generasi lama.
             </div>
             <br>
-        </div>
+        </div> --}}
         <br>
     </div>
     {{-- <br>
