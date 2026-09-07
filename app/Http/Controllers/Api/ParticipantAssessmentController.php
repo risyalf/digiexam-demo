0ff355675ff810f35fd261af5f007ee69e86aa13
risyalf <risyalfebrianto97@gmail.com>
2026-09-07 09:46:11 +0700

    restart

--- app/Http/Controllers/Api/ParticipantAssessmentController.php
+++ app/Http/Controllers/Api/ParticipantAssessmentController.php
@@ -61,6 +61,7 @@ class ParticipantAssessmentController extends Controller
                 'is_lock_enabled' => $assessment->is_lock_enabled
             ];
 
+
             return response()->json([
                 'message' => "SUKSES GET DATA SISWA",
                 'data' => $response
