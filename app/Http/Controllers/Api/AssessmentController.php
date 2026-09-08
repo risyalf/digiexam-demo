1cc4ffc5ce666037ba7deda7519166fef083753f
risyalf <risyalfebrianto97@gmail.com>
2026-09-08 08:12:56 +0700

    fix

--- app/Http/Controllers/Api/AssessmentController.php
+++ app/Http/Controllers/Api/AssessmentController.php
@@ -275,7 +275,7 @@ class AssessmentController extends Controller
                 "participant_assessment_id" => "required|uuid",
                 "value" => "array",
                 "value.*.test_question_id" => "uuid",
-                "value.*.answer" => "required",
+                // "value.*.answer" => "nullable",
                 // "value" => "required|array|min:1",
                 // "value.*.test_question_id" => "required|uuid",
                 // "value.*.answer" => "nullable|int",
