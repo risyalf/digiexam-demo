848b5932e88877dbb4a42e0c4dbc15d0e0356bc4
risyalf <risyalfebrianto97@gmail.com>
2026-09-11 21:49:18 +0700

    fix

--- app/Filament/Pages/EvaluateEssayAnswer.php
+++ app/Filament/Pages/EvaluateEssayAnswer.php
@@ -173,7 +173,7 @@ class EvaluateEssayAnswer extends Page implements HasTable, HasForms
                     ->when($this->filterFormData['module_id'], fn($q, $v) => $q->whereHas('assessment', fn($q) => $q->where('module_id', $v)))
                     ->when($this->filterFormData['topic_id'], fn($q, $v) => $q->whereHas('assessment', fn($q) => $q->where('topic_id', $v)))
                     ->when($this->filterFormData['group_id'], fn($q, $v) => $q->whereHas('participant', fn($q) => $q->where('group_id', $v)))
-                    ->when($this->filterFormData['assessment_id'], fn($q, $v) => $q->where('assessment_id', $v)))
+                    ->when($this->filterFormData['assessment_id'], fn($q, $v) => $q->where('assessment_id', $v))
                     ->when(Auth::user()->hasRole('guru'), function ($q) {
                         $userTopicIds = UserTopic::query()
                             ->where('user_id', Auth::id())
@@ -187,7 +187,10 @@ class EvaluateEssayAnswer extends Page implements HasTable, HasForms
                         fn($q, $v) =>
                         $q->whereHas('assessment.participant_groups', fn($q) => $q->where('participant_group_id', $v))
                     )
-                    ->when($this->filterFormData['assessment_id'], fn($q, $v) => $q->where('assessment_id', $v))
+                    ->when(
+                        $this->filterFormData['assessment_id'],
+                        fn($q, $v) => $q->where('assessment_id', $v)
+                    )
             )
             ->columns([
                 TextColumn::make('participant.user.name')
