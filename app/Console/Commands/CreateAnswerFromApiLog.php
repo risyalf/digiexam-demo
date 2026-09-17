347f40d34bbdb0c982459f0357abb8a2fa78dfc1
risyalf <risyalfebrianto97@gmail.com>
2026-09-17 09:39:53 +0700

    create answer from api logs

--- app/Console/Commands/CreateAnswerFromApiLog.php
+++ app/Console/Commands/CreateAnswerFromApiLog.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Enum\ParticipantStatus;
 use App\Models\Answer;
 use App\Models\ApiLog;
 use App\Models\ParticipantAssessment;
@@ -17,7 +18,7 @@ class CreateAnswerFromApiLog extends Command
      *
      * @var string
      */
-    protected $signature = 'create-answer-from-api-log {api_log_id}';
+    protected $signature = 'create-answer-from-api-log';
 
     /**
      * The console command description.
@@ -31,96 +32,111 @@ class CreateAnswerFromApiLog extends Command
      */
     public function handle()
     {
-        $apiLogId = $this->argument('api_log_id');
+        $assessmentIds = ParticipantAssessment::query()
+            ->select('participant_assessments.id')
+            ->join('participants', 'participants.id', '=', 'participant_assessments.participant_id')
+            ->where('participant_assessments.status', ParticipantStatus::SUBMITTED)
+            ->doesntHave('answers')
+            ->pluck('id');
 
-        $log = ApiLog::findOrFail($apiLogId);
+        foreach ($assessmentIds as $key => $id) {
+            $log = ApiLog::query()
+                ->where('json', 'like', "%$id%")
+                ->orderBy('created_at', 'desc')
+                ->first();
 
-        $str = $log->json;
-        $json = json_decode($str, true);
-        $participantAssessmentId = $json['participant_assessment_id'];
-        $value = $json['value'];
-
-        $answers = collect($value);
-
-        $questionIds = $answers->pluck("test_question_id")->unique();
-
-        $participantAssessment = ParticipantAssessment::findOrFail($participantAssessmentId);
-        $assessment = $participantAssessment->assessment;
-        $testId = $assessment->test_id;
-        $validQuestions = TestQuestion::query()
-            ->where("test_id", $testId)
-            ->whereIn("id", $questionIds)
-            ->select([
-                "id",
-                "type"
-            ])->get();
-
-        $validQuestionIds = $validQuestions->pluck('id')->toArray();
-
-        $correctOptions = TestQuestionOption::query()
-            ->whereIn("test_question_id", $validQuestionIds)
-            ->where("value", true)
-            ->get()
-            ->keyBy("test_question_id");
-
-        $correct = 0;
-        $wrong = 0;
-        $essayAnswers = [];
-
-        foreach ($answers as $item) {
-            if (!in_array($item["test_question_id"], $validQuestionIds)) {
+            if (!$log) {
+                $this->error("NO API LOG FOUND FOR ASSESSMENT $id");
                 continue;
             }
 
-            $answer = $item["answer"];
+            $str = $log->json;
+            $json = json_decode($str, true);
+            $participantAssessmentId = $json['participant_assessment_id'];
+            $value = $json['value'];
 
-            if ($validQuestions->where('id', $item["test_question_id"])->first()->type == 'Esai') {
-                $essayAnswers[] = [
-                    "test_question_id" => $item['test_question_id'],
-                    "value" => $answer,
-                    "evaluated" => false,
-                    "point" => 0
-                ];
-                continue;
+            $answers = collect($value);
+
+            $questionIds = $answers->pluck("test_question_id")->unique();
+
+            $participantAssessment = ParticipantAssessment::findOrFail($participantAssessmentId);
+            $assessment = $participantAssessment->assessment;
+            $testId = $assessment->test_id;
+            $validQuestions = TestQuestion::query()
+                ->where("test_id", $testId)
+                ->whereIn("id", $questionIds)
+                ->select([
+                    "id",
+                    "type"
+                ])->get();
+
+            $validQuestionIds = $validQuestions->pluck('id')->toArray();
+
+            $correctOptions = TestQuestionOption::query()
+                ->whereIn("test_question_id", $validQuestionIds)
+                ->where("value", true)
+                ->get()
+                ->keyBy("test_question_id");
+
+            $correct = 0;
+            $wrong = 0;
+            $essayAnswers = [];
+
+            foreach ($answers as $item) {
+                if (!in_array($item["test_question_id"], $validQuestionIds)) {
+                    continue;
+                }
+
+                $answer = $item["answer"];
+
+                if ($validQuestions->where('id', $item["test_question_id"])->first()->type == 'Esai') {
+                    $essayAnswers[] = [
+                        "test_question_id" => $item['test_question_id'],
+                        "value" => $answer,
+                        "evaluated" => false,
+                        "point" => 0
+                    ];
+                    continue;
+                }
+
+                $correctOption = $correctOptions[$item["test_question_id"]] ?? null;
+
+                if ($correctOption && $correctOption->id == $answer) {
+                    $correct++;
+                } else {
+                    $wrong++;
+                }
             }
 
-            $correctOption = $correctOptions[$item["test_question_id"]] ?? null;
+            $jsonValue = json_encode($value);
+            $essayValue = json_encode($essayAnswers);
+            $containEssay = count($essayAnswers) > 0;
+            DB::transaction(function () use ($correct, $wrong, $participantAssessment, $assessment, $jsonValue, $essayValue, $containEssay) {
+                $totalQuestion = $assessment->total_question;
+                $null = $totalQuestion - ($correct + $wrong);
 
-            if ($correctOption && $correctOption->id == $answer) {
-                $correct++;
-            } else {
-                $wrong++;
-            }
+                Answer::updateOrCreate(
+                    [
+                        "participant_assessment_id" => $participantAssessment->id,
+                    ],
+                    [
+                        "correct_answers" => $correct,
+                        "wrong_answers" => $wrong,
+                        "null_answers" => $null,
+                        "value" => $jsonValue,
+                        "essay_values" => $essayValue,
+                    ],
+                );
+
+                $point = 0;
+
+                if (!$containEssay) {
+                    $point = $correct / $totalQuestion * 100;
+                }
+
+                $participantAssessment->point = $point;
+                $participantAssessment->save();
+            });
         }
-
-        $jsonValue = json_encode($value);
-        $essayValue = json_encode($essayAnswers);
-        $containEssay = count($essayAnswers) > 0;
-        DB::transaction(function () use ($correct, $wrong, $participantAssessment, $assessment, $jsonValue, $essayValue, $containEssay) {
-            $totalQuestion = $assessment->total_question;
-            $null = $totalQuestion - ($correct + $wrong);
-
-            Answer::updateOrCreate(
-                [
-                    "participant_assessment_id" => $participantAssessment->id,
-                ],
-                [
-                    "correct_answers" => $correct,
-                    "wrong_answers" => $wrong,
-                    "null_answers" => $null,
-                    "value" => $jsonValue,
-                    "essay_values" => $essayValue,
-                ],
-            );
-
-            $point = 0;
-
-            if (!$containEssay) {
-                $point = $correct / $totalQuestion * 100;
-            }
-
-            $participantAssessment->point = $point;
-            $participantAssessment->save();
-        });
     }
 }
